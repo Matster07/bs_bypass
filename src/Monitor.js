823c9d610845a43e3b437636c2e60d5f001ee4d3
Matster07 <matveystepin07@gmail.com>
2020-12-08 12:19:53 +0300

    gfdg

--- src/Monitor.js
+++ src/Monitor.js
@@ -60,7 +60,7 @@ class Monitor {
             };
 
             const optionsForCookies = {
-                url: this.site,
+                url: "https://brandshop.ru/new/?#1",
                 headers: {
                     'User-Agent': 'Mozilla Firefox 36 (Win 8.1 x64): Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
